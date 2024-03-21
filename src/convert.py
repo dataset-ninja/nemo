ad3575cf7ce6d1ac83e60b9ea3529092a1663754
peixed351 <peixed351@gmail.com>
2024-03-21 17:46:23 +0300

    add settings

--- src/convert.py
+++ src/convert.py
@@ -1,17 +1,19 @@
 import os
 import shutil
+from collections import defaultdict
 
 import supervisely as sly
+from dataset_tools.convert import unpack_if_archive
 from supervisely.io.fs import (
     file_exists,
+    get_file_ext,
     get_file_name,
     get_file_name_with_ext,
-    get_file_size,
 )
+from supervisely.io.json import load_json_file
 from tqdm import tqdm
 
 import src.settings as s
-from dataset_tools.convert import unpack_if_archive
 
 
 def convert_and_upload_supervisely_project(
@@ -19,72 +21,107 @@ def convert_and_upload_supervisely_project(
 ) -> sly.ProjectInfo:
     # Possible structure for bbox case. Feel free to modify as you needs.
 
-    root_path = ""
-    images_folder = "images"
-    bboxes_folder = "labels"
+    train_images_path = "/home/alex/DATASETS/TODO/Nevada Smoke/archive/train_images"
+    val_images_path = "/home/alex/DATASETS/TODO/Nevada Smoke/archive/val_images"
     batch_size = 30
-    img_ext = ".png"
-    ann_ext = ".txt"
+    ann_train_path = "/home/alex/DATASETS/TODO/Nevada Smoke/archive/annotation_train.json"
+    ann_val_path = "/home/alex/DATASETS/TODO/Nevada Smoke/archive/annotation_val.json"
+
+    ds_name_to_data = {
+        "train": (train_images_path, ann_train_path),
+        "val": (val_images_path, ann_val_path),
+    }
 
     def create_ann(image_path):
-        labels, img_tags, label_tags = [], [], []
+        labels = []
+        tags = []
 
-        image_np = sly.imaging.image.read(image_path)[:, :, 0]
-        img_height = image_np.shape[0]
-        img_width = image_np.shape[1]
+        image_name = get_file_name_with_ext(image_path)
+        shape = image_name_to_shape.get(image_name)
+        if shape is not None:
+            img_height = shape[0]
+            img_wight = shape[1]
 
-        file_name = get_file_name(image_path)
-        curr_anns_dirpath = ""
-        ann_path = os.path.join(curr_anns_dirpath, file_name + ann_ext)
+        else:
+            image_np = sly.imaging.image.read(image_path)[:, :, 0]
+            img_height = image_np.shape[0]
+            img_wight = image_np.shape[1]
 
-        if file_exists(ann_path):
-            with open(ann_path) as f:
-                content = f.read().split("\n")
-                for curr_data in content:
-                    if len(curr_data) != 0:
-                        curr_data = list(map(float, curr_data.split(" ")))
+        ann_data = image_name_to_ann_data[get_file_name_with_ext(image_path)]
 
-                        left = int((curr_data[1] - curr_data[3] / 2) * img_width)
-                        right = int((curr_data[1] + curr_data[3] / 2) * img_width)
-                        top = int((curr_data[2] - curr_data[4] / 2) * img_height)
-                        bottom = int((curr_data[2] + curr_data[4] / 2) * img_height)
+        for curr_ann_data in ann_data:
+            category_id = curr_ann_data[0]
+            obj_class = idx_to_obj_class[category_id]
 
-                        rectangle = sly.Rectangle(top=top, left=left, bottom=bottom, right=right)
+            bbox_coord = curr_ann_data[1]
+            rectangle = sly.Rectangle(
+                top=int(bbox_coord[1]),
+                left=int(bbox_coord[0]),
+                bottom=int(bbox_coord[1] + bbox_coord[3]),
+                right=int(bbox_coord[0] + bbox_coord[2]),
+            )
+            label_rectangle = sly.Label(rectangle, obj_class)
+            labels.append(label_rectangle)
 
-                        for obj_class in obj_classes:
-                            if obj_class.name == idx2clsname[curr_data[0]]:
-                                curr_obj_class = obj_class
-                                break
-                        label = sly.Label(rectangle, curr_obj_class, label_tags)
-                        labels.append(label)
-
-        return sly.Annotation(img_size=(img_height, img_width), labels=labels, img_tags=img_tags)
-
-    class_names = ["class1", "class2", ...]
-    idx2clsname = {}
-    obj_classes = [sly.ObjClass(name, sly.Rectangle) for name in class_names]
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels, img_tags=tags)
 
     project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
-    meta = sly.ProjectMeta(obj_classes=obj_classes)
+
+    idx_to_obj_class = {
+        1: sly.ObjClass("low smoke", sly.Rectangle),
+        3: sly.ObjClass("mid smoke", sly.Rectangle),
+        5: sly.ObjClass("high smoke", sly.Rectangle),
+    }
+
+    meta = sly.ProjectMeta(obj_classes=list(idx_to_obj_class.values()))
+
     api.project.update_meta(project.id, meta.to_json())
 
-    for ds_name in os.listdir(root_path):
+    for ds_name, ds_data in ds_name_to_data.items():
+
         dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
-        dataset_path = os.path.join(root_path, ds_name)
 
-        images_pathes = sly.fs.list_files_recursively(dataset_path, valid_extensions=[img_ext])
+        images_path, ann_json_path = ds_data
 
-        pbar = tqdm(desc=f"Create dataset '{ds_name}'", total=len(images_pathes))
-        for images_pathes_batch in sly.batched(images_pathes, batch_size=batch_size):
-            images_names_batch = [
-                get_file_name_with_ext(image_path) for image_path in images_pathes_batch
+        ann = load_json_file(ann_json_path)
+
+        image_id_to_name = {}
+        image_name_to_ann_data = defaultdict(list)
+        image_name_to_shape = {}
+
+        for curr_image_info in ann["images"]:
+            image_id_to_name[curr_image_info["id"]] = curr_image_info["file_name"].split("/")[-1]
+            image_name_to_shape[curr_image_info["file_name"].split("/")[-1]] = (
+                curr_image_info["height"],
+                curr_image_info["width"],
+            )
+
+        for curr_ann_data in ann["annotations"]:
+            image_id = curr_ann_data["image_id"]
+            image_name_to_ann_data[image_id_to_name[image_id]].append(
+                [
+                    curr_ann_data["category_id"],
+                    curr_ann_data["bbox"],
+                ]
+            )
+
+        images_names = [
+            im_name for im_name in os.listdir(images_path) if get_file_ext(im_name) == ".jpg"
+        ]
+
+        progress = sly.Progress("Create dataset {}".format(ds_name), len(images_names))
+
+        for images_names_batch in sly.batched(images_names, batch_size=batch_size):
+            img_pathes_batch = [
+                os.path.join(images_path, image_name) for image_name in images_names_batch
             ]
 
-            img_infos = api.image.upload_paths(dataset.id, images_names_batch, images_pathes_batch)
-            img_ids = [image.id for image in img_infos]
+            img_infos = api.image.upload_paths(dataset.id, images_names_batch, img_pathes_batch)
+            img_ids = [im_info.id for im_info in img_infos]
 
-            anns = [create_ann(image_path) for image_path in images_pathes_batch]
+            anns = [create_ann(image_path) for image_path in img_pathes_batch]
             api.annotation.upload_anns(img_ids, anns)
 
-            pbar.update(len(images_names_batch))
+            progress.iters_done_report(len(images_names_batch))
+
     return project
--- src/settings.py
+++ src/settings.py
@@ -13,56 +13,71 @@ from dataset_tools.templates import (
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "NEMO"
+PROJECT_NAME_FULL: str = "NEMO: Nevada Smoke Detection Dataset"
 HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-APPLICATIONS: List[Union[Industry, Domain, Research]] = None
-CATEGORY: Category = None
+LICENSE: License = License.Apache_2_0(
+    source_url="https://github.com/SayBender/Nemo/blob/main/LICENSE"
+)
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Environmental()]
+CATEGORY: Category = Category.Environmental()
 
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+CV_TASKS: List[CVTask] = [CVTask.ObjectDetection(), CVTask.Identification()]
+ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.ObjectDetection()]
 
 RELEASE_DATE: Optional[str] = None  # e.g. "YYYY-MM-DD"
 if RELEASE_DATE is None:
-    RELEASE_YEAR: int = None
+    RELEASE_YEAR: int = 2022
 
-HOMEPAGE_URL: str = None
+HOMEPAGE_URL: str = "https://github.com/SayBender/Nemo"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 16211695
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = None
+GITHUB_URL: str = "https://github.com/dataset-ninja/nemo"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = (
+    "https://www.kaggle.com/datasets/werus23/nevada-smoke-detection-data/download?datasetVersionNumber=1"
+)
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
-CLASS2COLOR: Optional[Dict[str, List[str]] | Literal["predefined"]] = "predefined"
+CLASS2COLOR: Optional[Dict[str, List[str]] or Literal["predefined"]] = {
+    "low smoke": [230, 25, 75],
+    "mid smoke": [60, 180, 75],
+    "high smoke": [255, 225, 25],
+}
 # If specific colors for classes are needed, fill this dict (e.g. {"class1": [255, 0, 0], "class2": [0, 255, 0]})
 
 # If you have more than the one paper, put the most relatable link as the first element of the list
 # Use dict key to specify name for a button
-PAPER: Optional[Union[str, List[str], Dict[str, str]]] = None
+PAPER: Optional[Union[str, List[str], Dict[str, str]]] = "https://www.mdpi.com/2072-4292/14/16/3979"
 BLOGPOST: Optional[Union[str, List[str], Dict[str, str]]] = None
-REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = {
-    "GitHub": "some_link_to_repo_if_exists"
-}
+REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = None
 
 CITATION_URL: Optional[str] = None
-AUTHORS: Optional[List[str]] = None
-AUTHORS_CONTACTS: Optional[List[str]] = None
+AUTHORS: Optional[List[str]] = [
+    "Amirhessam Yazdi",
+    "Heyang Qin",
+    "Connor Jordan",
+    "Lei Yang",
+    "Feng Yan",
+]
+AUTHORS_CONTACTS: Optional[List[str]] = ["ayazdi@nevada.unr.edu"]
 
-ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
-ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
+ORGANIZATION_NAME: Optional[Union[str, List[str]]] = [
+    "University of Nevada, USA",
+    "University of Houston, USA",
+]
+ORGANIZATION_URL: Optional[Union[str, List[str]]] = ["https://www.unr.edu/", "https://www.uh.edu/"]
 
 # Set '__PRETEXT__' or '__POSTTEXT__' as a key with string value to add custom text. e.g. SLYTAGSPLIT = {'__POSTTEXT__':'some text}
 SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = None
